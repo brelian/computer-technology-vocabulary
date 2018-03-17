de0a16ab0760d35f808ca3735befc4c71b0a8648
root <root@localhost.localdomain>
2018-03-17 07:51:39 +0000

    add new word

--- build/index.php
+++ build/index.php
@@ -4,14 +4,15 @@ define('APP_PATH', dirname(__DIR__));
 $keyword = $argv[1];
 
 // if results is empty, using anather API
-$url      = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' . $keyword . '&limit=1&namespace=0&format=json';
+$url      = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' . $keyword . '&limit=3&namespace=0&format=json';
 $response = json_decode(file_get_contents($url), true);
 if (!empty($response[1][0]) && !empty($response[2][0])) {
-    echo 'from widipedia \n';
+    echo "from widipedia \n";
     $word       = $response[1][0];
-    $definition = $response[2][0];
+    $definition = ($response[2][0] == $word." may refer to:") ? $response[2][1] : $response[2][0];
+    // $definition = $response[2][0];
 } else {
-    echo 'from pearson';
+    echo "from pearson \n";
     $url      = 'http://api.pearson.com/v2/dictionaries/entries?headword=' . $keyword;
     $response = json_decode(file_get_contents($url), true);
 
@@ -19,15 +20,20 @@ if (!empty($response[1][0]) && !empty($response[2][0])) {
         foreach ($response['results'] as $value) {
             if (!empty($value['senses'][0]['definition'][0])) {
                 $senses[] = is_array($value['senses'][0]['definition']) ? $value['senses'][0]['definition'][0] : $value['senses'][0]['definition'];
+                $headword[] = $value['headword'];
             }
         }
 
         $definition = $senses[0];
-        $word       = $response['results'][0]['headword'];
+        $word       = $headword[0];
     }
     // print_r($senses);
 }
 
-$audioUrl = 'http://dict.youdao.com/dictvoice?audio=' . $word;
-$row      = "\n| $word |[:speaker:]($audioUrl) | $definition |      |      |";
-file_put_contents(APP_PATH . '/README.md', $row, FILE_APPEND);
+if (!empty($word) && !empty($definition)) {
+	$audioUrl = 'http://dict.youdao.com/dictvoice?audio=' . $word;
+	$row      = "\n| $word |[:speaker:]($audioUrl) | $definition |      |      |";
+	file_put_contents(APP_PATH . '/README.md', $row, FILE_APPEND);
+} else {
+	echo "Not find ".$keyword;
+}
